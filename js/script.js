3b8dac3ea2717bf6db2b903ba374e666d2d9725c
Dexter84 <fenixzaec84@gmail.com>
2024-11-10 14:08:32 +0700

    lesson15

---
+++ js/script.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const container = document.querySelector(".container");
+
+const DomElement = function(selector, height, width, bg, fsz) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fsz = fsz;
+
+    this.createElement = function () {
+        let elem = document.createElement('div');
+        if (selector[0] === '.') {
+            elem.innerHTML = '<div></div>'
+            elem.classList.add(selector.slice(1))
+        } else if (selector[0] === '#') {
+            elem.innerHTML = '<p></p>'
+            elem.id = (selector);
+        }
+        elem.textContent = "Привет, я любой текст";
+        elem.style.cssText = `
+            height: ${height};
+            width: ${width};
+            background: ${bg};
+            font-size: ${fsz}
+        `;
+        return elem;
+    };
+}
+
+const newElem = new DomElement("#id", "50px", "100%", "#ff7800", "30px");
+
+container.appendChild(newElem.createElement());
